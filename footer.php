31e23a9f9d20f94a62b648c1952354f8bb8ece36
Kenji <kenji.au9@gmail.com>
2020-10-07 11:18:55 -0600

    Added footer;

---
+++ footer.php
@@ -0,0 +1,20 @@
+ <footer>
+   <h2>
+     <?php bloginfo( 'title' ); ?>Footer
+   </h2>
+   <p>
+      &copy;
+      <?php echo date( 'Y' ); ?>
+      <a href="<?php echo site_url(); ?>">
+        <?php bloginfo( 'title' ); ?>
+      </a>
+      All Rights reserved.
+   </p>
+ </footer>
+ <?php
+  // Similar to wp_head(), but for footer scripts
+  // and output
+  wp_footer();
+  ?>
+</body>
+</html>
