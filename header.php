ecc00605693838022d07c129f419a62e598b1473
Kenji <kenji.au9@gmail.com>
2020-10-07 11:07:35 -0600

    Added in header;

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo('title'); ?></title>
+  <?php
+  // This will output any scripts, stytles, meta and more 
+  // that our theme / plugin need
+  wp_head();
+  ?>
+</head>
+
+<body>
+  <h1><?php bloginfo('title'); ?></h1>
