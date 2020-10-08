efa87523dc147b65e0c8699da14a620af91b37f9
Kenji <kenji.au9@gmail.com>
2020-10-07 21:51:08 -0600

    Fixed function file; Script outputs to console correctly;

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
- /*
+/*
   * Register a navigation menu.
   */
 register_nav_menus(
@@ -8,5 +8,31 @@ register_nav_menus(
     'footer_menu' => 'Footer Menu',
 
   )
-)
-?>
+);
+/**
+ * Enqueue theme stylesheets and scripts.
+ */
+add_action( 'wp_enqueue_scripts', function()
+{
+  // Enqueue our JavaScript!
+  wp_enqueue_script(
+    // An ID / name for our script.
+    'techcareers-theme-scripts',
+    // The "public" path to the script file.
+    get_theme_file_uri( 'assets/js/scripts.js' ),
+    // Dependencies ( jQuery, BootStrap, etc.)
+    array(),
+    // Version number of the file.
+    strftime( get_theme_file_uri( '/assets/js/scripts.js' ) ),
+    // Should we output this script in the foor.php?
+    FALSE
+  );
+  // Enqueue our stylesheet!
+  wp_enqueue_style(
+    'techcareers-theme-main-styles',
+    get_theme_file_uri( 'assets/css/main.css' ),
+    strftime( get_theme_file_uri( '/assets/css/main.css' ) ),
+    // The type of media we're targetting (media query)
+    'all'
+  );
+} );
